8e2f83f25c3369b18aeb7afc40cd3092a21165ef
sugarmouse <tanghao19950911@gmail.com>
2022-11-27 22:47:24 +0800

    implement chatList UI

---
+++ src/components/ChatListItem/index.js
@@ -0,0 +1,58 @@
+import { Text, View, Image, StyleSheet } from 'react-native'
+
+const ChatListItem = () => {
+  return (
+    <View style={styles.container} >
+      <Image
+        source={{
+          uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/lukas.jpeg",
+        }}
+        style={styles.image}
+      />
+
+      <View style={styles.content}>
+        <View style={styles.row} >
+          <Text style={styles.name}>Lucas</Text>
+          <Text style={styles.subTitle}>8:30</Text>
+        </View>
+
+        <Text style={styles.message}>Hello there</Text>
+
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    alignItems: "stretch",
+    marginHorizontal: 10,
+    marginVertical: 5,
+    height: 70,
+  },
+  image: {
+    width: 60,
+    aspectRatio: 1,
+    borderRadius: 30,
+    marginRight: 10,
+  },
+  content: {
+    flex: 1,
+    borderBottomColor: "lightgray",
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+  row: {
+    flexDirection: "row",
+    marginBottom: 5,
+  },
+  name: {
+    fontWeight: "bold",
+    flex: 1,
+  },
+  subTitle: {
+    color: "grey",
+  },
+})
+
+export default ChatListItem
