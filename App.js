00086ba3bfff90c92ca6d5d31096837bb0fdfab6
sugarmouse <tanghao19950911@gmail.com>
2022-11-28 14:53:03 +0800

    change file name

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
-import ChatsScreen from './src/screens/ChatScreen'
+import ChatsScreen from './src/screens/ChatsScreen'
 
 export default function App() {
   return (
