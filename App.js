52e426856b92c6790a8400719e43c9dba1839ec8
sugarmouse <tanghao19950911@gmail.com>
2022-11-28 14:45:03 +0800

    implement chatScreen with dummy data

--- App.js
+++ App.js
@@ -1,24 +1,11 @@
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
-import ChatListItem from './src/components/ChatListItem'
-
-const chat = {
-  id: "1",
-  user: {
-    image:
-      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/lukas.jpeg",
-    name: "shengzeqiang",
-  },
-  lastMessage: {
-    text: "Oke",
-    createdAt: "07:30",
-  },
-}
+import ChatsScreen from './src/screens/ChatScreen'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <ChatListItem chat={chat} />
+      <ChatsScreen />
       <StatusBar style="auto" />
     </View>
   )
@@ -28,7 +15,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'white',
-    alignItems: 'center',
+    // alignItems: 'center',
     justifyContent: 'center',
+    paddingVertical: 50,
   },
 })
