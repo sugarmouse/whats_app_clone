1a7c3513f6c3a6ef34be8824100b0177a55aecc8
sugarmouse <tanghao19950911@gmail.com>
2022-11-28 15:58:22 +0800

    implement chat screen UI

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
+import ChatScreen from './src/screens/ChatScreen'
 import ChatsScreen from './src/screens/ChatsScreen'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <ChatsScreen />
+      {/* <ChatsScreen /> */}
+      <ChatScreen />
       <StatusBar style="auto" />
     </View>
   )
---
+++ src/screens/ChatScreen.jsx
@@ -0,0 +1,35 @@
+import {
+  View,
+  ImageBackground,
+  Text,
+  StyleSheet,
+  FlatList,
+} from "react-native";
+import bg from "../../assets/images/BG.png";
+import Message from "../components/Message";
+import messages from "../../assets/data/messages.json";
+
+const renderItem = (message) => {
+  return <Message message={message.item} />;
+};
+
+const ChatScreen = () => {
+  return (
+    <ImageBackground source={bg} style={styles.bg}>
+      <FlatList
+        data={messages}
+        renderItem={renderItem}
+        style={{ padding: 10 }}
+        inverted
+      />
+    </ImageBackground>
+  );
+};
+
+const styles = StyleSheet.create({
+  bg: {
+    flex: 1,
+  },
+});
+
+export default ChatScreen;
